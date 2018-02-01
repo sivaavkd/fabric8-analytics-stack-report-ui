60d4c48a9570d0920957473ad897b3e5ad6c43e5
Jyasveer Gotta <jgotta@redhat.com>
2018-02-01 19:45:30 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 // The usual bootstrapping imports
 
 // import './polyfills';
+import 'core-js/es6';
 import 'core-js/es7/reflect';
 import 'zone.js';
 import { enableProdMode } from '@angular/core';
